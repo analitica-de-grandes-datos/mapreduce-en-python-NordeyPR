787937a0813d109f5f5b7caa953cfb7fba393d54
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-12 14:27:23 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    lista1 = []
+    #
+    # cada linea de texto recibida se adiciona a la lista1
+    #
+    for line in sys.stdin:
+        line1 = line.replace('\n','').split(',')
+        line1[2] = int(line1[2])
+        lista1.append(line1)
+       
+
+
+    lista2 = sorted(lista1, key=lambda x:x[2])
+    lista2 = lista2[0:6]
+
+    for item in lista2:
+        sys.stdout.write("{}   {}   {}\n".format(item[0],item[1],item[2]))

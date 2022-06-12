63669091521ba56fa9eba11fc21456c3b3cc94e3
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-12 14:27:17 -0500

    Update reducer.py

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from itertools import count
+import numpy
+import sys
+if __name__ == '__main__':
+    curkey = None
+    total1 = 0
+    total2 = 0
+    letra = 0
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+            letra += 1
+            total1 += val
+            total2 = total1 / letra
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1, total2))
+            curkey = key
+            letra = 1
+            total1 = val
+            total2 = total1 / letra
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1 , total2))

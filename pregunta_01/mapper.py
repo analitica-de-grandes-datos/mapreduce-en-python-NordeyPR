e7e562fc4a9613c0c514d0e2eae2fca7d76cb09e
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-12 14:26:21 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+
+#### Importamos libreriass
+
+#! /usr/bin/ python3
+
+import sys
+
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        result = line.split(",")
+                
+        sys.stdout.write("{}\t1\n".format(result[2]))

80d6492c8b84fee7be69c2dba79e738d04c60752
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-12 14:26:51 -0500

    Update reducer.py

--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,27 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    curkey = None
+    total1 = 0
+    total2 = 0
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+
+        if key == curkey:
+             if val > total1:
+                 total1 = val
+                 if key == curkey:
+                    if val < total1:
+                        total2 = val
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1 , total2))
+            curkey = key
+            total1 = val
+            total2 = val
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, total1 , total2))

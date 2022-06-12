23928c3a34de6601a6437545312f18fb63a29b71
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-12 14:27:28 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    curkey = "A"
+    total = 0
+    #
+    # cada linea de texto recibida se adiciona a la lista1
+    #
+    for line in sys.stdin:
+        val = line[1]
+        key = line[3].split(",")
+        val = (val)
+        print(key ,  val)
+            #if key == curkey:
+                #total.append(val)
+                #print(total)

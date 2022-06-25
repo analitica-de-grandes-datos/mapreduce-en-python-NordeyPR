c8f2171c2d10df51cb3e525e4c697d9ccb0933ce
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-24 19:48:46 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -2,18 +2,34 @@
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
 import sys
+
 if __name__ == '__main__':
 
-    curkey = "A"
-    total = 0
-    #
-    # cada linea de texto recibida se adiciona a la lista1
-    #
+    lista1 = []
+    lista3 = []
+   
     for line in sys.stdin:
-        val = line[1]
-        key = line[3].split(",")
-        val = (val)
-        print(key ,  val)
-            #if key == curkey:
-                #total.append(val)
-                #print(total)
+        line1 = line.replace('\n','').split(',')
+        
+        lista1.append(line1)
+        
+
+        letras=list(set([z[1] for z in lista1]))
+        letras.sort()
+
+    for letra in letras:
+        lista2=[]
+        for element in lista1:
+            for i in element[1:]:
+                    
+
+                if i == letra:
+                    lista2.append(int(element[0]))
+        string=','.join(str(e) for e in sorted(lista2))    
+
+        tupla=letra,string
+        lista3=lista3+[tupla]
+
+   
+    for item in lista3:
+        sys.stdout.write("{}\t{}\n".format(item[0],item[1]))

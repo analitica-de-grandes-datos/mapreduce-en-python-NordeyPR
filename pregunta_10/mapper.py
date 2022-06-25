7aa80318b80cd705983700f5eac50640c5902df8
NordeyPR <102186979+NordeyPR@users.noreply.github.com>
2022-06-24 19:48:19 -0500

    Update mapper.py

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -2,7 +2,6 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-import pandas
 
 if __name__ == "__main__":
 
@@ -11,18 +10,7 @@ if __name__ == "__main__":
     for line in sys.stdin:
         result = line.split()
         
-        #result1 = result[0].split("\t")
-        result = line.replace("\t" , ",")
-        result1 = result.replace("\r" , "")
-        result2 = result1.replace("\n" , "")
-        result3 = result2.split(",")
-        clave = int(result3[0])
-        valor = sorted(result3[1:])
-        clave = clave
-        
-        #print(valor , clave)
-        
-        
+   
                 
         #sys.stdout.write("{}".format(result))
-        sys.stdout.write("{}\t{}\n".format(valor ,  clave))
+        sys.stdout.write("{},{}\n".format(result[0],result[1]))
